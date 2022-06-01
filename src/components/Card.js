6d533e373e96e05323f80ca8b53785ffbb04926a
faithofogba <onomeofogba13@gmail.com>
2022-06-01 02:06:31 +0100

    clean ups

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,12 +6,12 @@ export default function Card(person) {
       <div className="card-head d-flex flex-row">
         <img className="rounded-circle" src={person.image} alt="person"/>
         <section className="ms-3 p-10">
-          <h5 className={person.nameId}>{person.name}</h5>
+          <h1 className={person.nameClass}>{person.name}</h1>
           <p className="user-status">Verified Graduate</p>
         </section>
       </div>
       <section className="card-body p-0">
-        <h5 className={person.titleId}>{person.quoteHeader}</h5>
+        <h5 className={person.titleClass}>{person.quoteHeader}</h5>
         <p className="card-text">{person.quoteText}</p>
       </section>
     </div>
